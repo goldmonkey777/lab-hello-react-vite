0e412dee59a7863c79408559dbf67d60ceeb574b
Goldmonkey Studio <contact@goldmonkey.studio>
2026-02-04 21:02:03 +0100

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import logo from './assets/68747470733a2f2f73332d65752d776573742d312e616d617a6f6e6177732e636f6d2f69682d6d6174657269616c732f75706c6f6164732f75706c6f61645f32633563323465653035616135666136386137366565653536346164343465652e706e67.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="Logo" />
+      <h1>Hello React Landing Page</h1>
+      <p>Bem-vindo ao seu primeiro projeto React com Vite!</p>
+      <footer className="footer">feito por Goldmonkey Studio</footer>
+    </div>
+  );
+}
+
+export default App;
